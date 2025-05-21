85a2e9b5097cd95949c92bd76f98ddf0e245a3c6
CapacitaBR-2025-E19 <equipe19.ioscapacitabr@gmail.com>
2025-05-21 01:22:16 -0300

    initial

---
+++ teste_14-05-25/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  teste_14-05-25
+//
+//  Created by iredefbmac_29 on 14/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ teste_14-05-25/teste_14_05_25App.swift
@@ -0,0 +1,17 @@
+//
+//  teste_14_05_25App.swift
+//  teste_14-05-25
+//
+//  Created by iredefbmac_29 on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct teste_14_05_25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
